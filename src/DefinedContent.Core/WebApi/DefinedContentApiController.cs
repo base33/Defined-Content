d8e942f7f9910643dc46490944ab59ac3399310c
David Greasley <d.greasley@mentordigital.co.uk>
2015-03-09 19:48:20 +0000

    Adding missing files.

---
+++ src/DefinedContent.Core/WebApi/DefinedContentApiController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace DefinedContent.WebApi
+{
+	public class DefinedContentApiController : UmbracoApiController
+	{
+		[HttpGet]
+		public bool FullRefresh()
+		{
+			try
+			{
+				DefinedContent.Current.FullRefresh();
+
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+
+		[HttpGet]
+		public int GetId(string key)
+		{
+			return DefinedContent.GetId(key);
+		}
+	}
+}
